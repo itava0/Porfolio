191153d74b8e4baa6f74ef291038bddd16bc95d8
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2021-05-20 21:12:26 -0400

    Updates

---
+++ precache-manifest.04a45bde32808c6ed7caa03cf419fed3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf7294858c1ce6afe58485d182ba517",
+    "url": "/Porfolio/index.html"
+  },
+  {
+    "revision": "5ac44406f3ab142efa5b",
+    "url": "/Porfolio/static/css/main.86ecf7e0.chunk.css"
+  },
+  {
+    "revision": "46bf04ca2080b4f2238c",
+    "url": "/Porfolio/static/js/2.f718aec5.chunk.js"
+  },
+  {
+    "revision": "5ac44406f3ab142efa5b",
+    "url": "/Porfolio/static/js/main.e2d6e6ee.chunk.js"
+  },
+  {
+    "revision": "ca7d3b8754bbcf63d22f",
+    "url": "/Porfolio/static/js/runtime~main.66c6aa23.js"
+  },
+  {
+    "revision": "30341deecfe38e99c7591a11a95de642",
+    "url": "/Porfolio/static/media/Onchill.30341dee.png"
+  },
+  {
+    "revision": "cd703c2e5365fc274e320aa9f6adb22a",
+    "url": "/Porfolio/static/media/RestaurantPassport.cd703c2e.png"
+  },
+  {
+    "revision": "308be61a043278c3bf2918b013c39812",
+    "url": "/Porfolio/static/media/soniry.308be61a.png"
+  }
+]);
