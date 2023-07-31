99c29070bc581cd5e2b358146be437070fda09bd
Ivan Tavarez <ivantavarez2@gmail.com>
2023-07-31 11:54:11 -0400

    Changed name

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -4,7 +4,7 @@ const Intro = () => {
   return (
     <div className="intro">
       <div>
-        <p>Welcome, my name is Igor Tavarez</p>
+        <p>Welcome, my name is Ivan Tavarez</p>
         <p className="subheading">I am a Software Developer based in Charlotte, NC.</p>
       </div>
     </div>
